28762607a1773d3d232721d2fdc54d2d29813f17
noah <noahbackes2@gmail.com>
2024-08-30 10:21:21 +0200

    Datagenerators Changes

--- src/main/java/be/noah/invasion/datagen/ModRecipeProvider.java
+++ src/main/java/be/noah/invasion/datagen/ModRecipeProvider.java
@@ -1,14 +1,20 @@
 package be.noah.invasion.datagen;
 
+import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
 
+import java.util.concurrent.CompletableFuture;
+
 public class ModRecipeProvider extends RecipeProvider implements IConditionBuilder {
-    public ModRecipeProvider(PackOutput pOutput) {
-        super(pOutput);
+    public ModRecipeProvider(PackOutput pOutput, CompletableFuture<HolderLookup.Provider> pRegistries) {
+        super(pOutput, pRegistries);
     }
+    /*public ModRecipeProvider(PackOutput pOutput) {
+        super(pOutput);
+    }*/
     @Override
     protected void buildRecipes(RecipeOutput pRecipeOutput) {
 
